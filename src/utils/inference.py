3592b78faca94b6533cc11e2bac1779463370e91
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2023-10-16 16:28:19 -0400

    ml face_classfication project

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -1,11 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.preprocessing import image
+from tensorflow.keras.utils import load_img, img_to_array
 
 def load_image(image_path, grayscale=False, target_size=None):
-    pil_image = image.load_img(image_path, grayscale, target_size)
-    return image.img_to_array(pil_image)
+    pil_image = load_img(image_path, grayscale=grayscale, color_mode='rgb', target_size=target_size)
+    return img_to_array(pil_image)
 
 def load_detection_model(model_path):
     detection_model = cv2.CascadeClassifier(model_path)
@@ -24,7 +24,7 @@ def apply_offsets(face_coordinates, offsets):
     return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
 
 def draw_text(coordinates, image_array, text, color, x_offset=0, y_offset=0,
-                                                font_scale=2, thickness=2):
+              font_scale=2, thickness=2):
     x, y = coordinates[:2]
     cv2.putText(image_array, text, (x + x_offset, y + y_offset),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -34,4 +34,3 @@ def get_colors(num_classes):
     colors = plt.cm.hsv(np.linspace(0, 1, num_classes)).tolist()
     colors = np.asarray(colors) * 255
     return colors
-
